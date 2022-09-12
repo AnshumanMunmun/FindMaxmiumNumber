d26eb5966fd31df41864bf08b7d512f418ca0053
AnshumanMunmun <imanshuman47@gmail.com>
2022-09-12 21:31:18 +0530

    [Anshuman] Added: Finding Three Maximum Floats

--- FindMaximumNumber/FindMaximumNumb.cs
+++ FindMaximumNumber/FindMaximumNumb.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace FindMaximumNumber
 {
-    public class FindMaximum
+    public class FindMaximumNumb
     {
         public int FindMaxIntNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+            else
+                return thirdNum;
+        }
+        public float FindMaxFloatNumber(float firstNum, float secondNum, float thirdNum)
         {
             if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
                 return firstNum;
--- FindMaximumNumberTest/UnitTest1.cs
+++ FindMaximumNumberTest/UnitTest1.cs
@@ -29,5 +29,26 @@ namespace FindMaximumNumberTest
             int num = max.FindMaxIntNumber(1, 2, 3);
             Assert.AreEqual(num, 3);
         }
+        [Test]
+        public void GivenMaxFolatsFirstNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximumNumb max = new FindMaximumNumb();
+            float num = max.FindMaxFloatNumber(3.1f, 1.2f, 2.4f);
+            Assert.AreEqual(num, 3.1f);
+        }
+        [Test]
+        public void GivenMaxFolatsSecondNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximumNumb max = new FindMaximumNumb();
+            float num = max.FindMaxFloatNumber(2.4f, 3.1f, 1.2f);
+            Assert.AreEqual(num, 3.1f);
+        }
+        [Test]
+        public void GivenMaxFolatsThirdNumber_WhenAnalized_ShouldReturnMaxNumber()
+        {
+            FindMaximumNumb max = new FindMaximumNumb();
+            float num = max.FindMaxFloatNumber(1.2f, 2.4f, 3.1f);
+            Assert.AreEqual(num, 3.1f);
+        }
     }
 }
